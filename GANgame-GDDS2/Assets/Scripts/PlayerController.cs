9e9c841aa212ed49bcfb229843c92a4cf017c8d4
schmidtheimer <104008594+schmidtheimer@users.noreply.github.com>
2022-04-24 00:12:12 +0800

    Added PlayerController
    Added Folders for script and sprite, added inventory subfolder to script folder

---
+++ GANgame-GDDS2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementHori(Input.GetAxis("Horizontal"));
+        MovementVert(Input.GetAxis("Vertical"));
+    }
+
+    public void MovementVert(float Direction)
+    {
+        transform.position += transform.up*moveSpeed*Direction*Time.deltaTime;
+    }
+    public void MovementHori(float Direction)
+    {
+        transform.position += transform.right * moveSpeed * Direction* Time.deltaTime;
+    }
+}
