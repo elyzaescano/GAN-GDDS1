68edb4e86c30e1dc15a420516ed3e91ec4d73552
schmidtheimer <medamanoh@live.com.sg>
2022-05-17 16:29:42 +0800

    Minor Changes
    Need to fix UI,  map event trigger

--- GANgame-GDDS2/Assets/SciptableObjects/Inventory/Scripts/InventoryObject.cs
+++ GANgame-GDDS2/Assets/SciptableObjects/Inventory/Scripts/InventoryObject.cs
@@ -78,7 +78,7 @@ public class InventoryObject : ScriptableObject, ISerializationCallbackReceiver
         }
         else
         {
-            Container.RemoveAt(i);
+            Container.RemoveAt(i);  
         }
     }
 
