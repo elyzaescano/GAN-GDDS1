b2e54933e9501e9bb8dca3c9e5ceb85da2968cd6
elyzaescano <93236863+elyzaescano@users.noreply.github.com>
2022-05-20 12:38:37 +0800

    keypad insert
    - Keypad functional, fully equipped with SFX
    - Visuals... not so much
    - Approx. 3 hate crimes committed
    - 12 people misgendered

---
+++ GANgame-GDDS2/Assets/UI Scripts/KeypadScreen.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class KeypadScreen : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI codeText;
+    string codeTextValue = "";
+    public string passCode;
+    bool canAddDigit;
+
+    AudioSource audioSource;
+    public AudioClip[] feedback; 
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();  
+    }
+    private void Update()
+    {
+        codeText.text = codeTextValue;
+
+        if (codeTextValue.Length == 4)
+        {
+            canAddDigit = false;
+        }
+        else
+        {
+            canAddDigit = true;
+        }
+
+        
+    }
+
+    public void AddDigit(string digit)
+    {
+        if (canAddDigit)
+        {
+            codeTextValue += digit;
+            audioSource.clip = feedback[2];
+            audioSource.Play();
+        }
+    }
+     public void ClearNumber()
+    {
+        codeTextValue = "";
+        audioSource.clip = feedback[3];
+        audioSource.Play();
+    }
+
+    public void EnterCode()
+    {
+        if (codeTextValue == passCode)
+        {
+            print("OPEN");
+            audioSource.clip = feedback[1];
+            audioSource.Play();
+        }
+        else
+        {
+            audioSource.clip = feedback[0];
+            audioSource.Play();
+            codeTextValue = "";
+        }
+    }
+}
