67e94762ba4ddb3890eda59b03c5b337fc1eeeb8
Greg <gregoryg.21@ichat.sp.edu.sg>
2022-05-29 20:40:19 +0800

    imported menu package
    -sound still somewhat broken but its condition is improving :'-)
    - added menus and settings scenes to build settings
    -hello

---
+++ GANgame-GDDS2/Assets/UI Scripts/AudioManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    public Music[] music;
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach(Music m in music)
+        {
+            m.source = gameObject.AddComponent<AudioSource>();
+            m.source.clip = m.clip;
+
+            m.source.volume = m.volume;
+            m.source.pitch = m.pitch;
+            m.source.loop = m.loop;
+            m.source.outputAudioMixerGroup = m.audioMixer;
+
+        }
+
+    }
+
+    public void Play(string name)
+    {
+        Music m = Array.Find(music, music => music.name == name);
+        if (m == null)
+            return;
+        m.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Music m = Array.Find(music, music => music.name == name);
+        if (m == null)
+            return;
+        m.source.Stop();
+    }
+
+
+
+}
+ 
