b847873047c723b215b5e68c3b706d305e650e2e
srsbsns5 <elyzaescano@gmail.com>
2022-05-29 21:49:03 +0800

    Enemy Part 2

--- GANgame-GDDS2/Assets/Script/Enemy/EnemyAnimationHandler.cs
+++ GANgame-GDDS2/Assets/Script/Enemy/EnemyAnimationHandler.cs
@@ -7,13 +7,13 @@ namespace EnemyAI
     public class EnemyAnimationHandler : MonoBehaviour
     {
         Animator anim;
-
         EnemyFieldOfView enemyFOV;
 
         void Awake()
         {
             anim = GetComponent<Animator>();
             enemyFOV = FindObjectOfType<EnemyFieldOfView>();
+
         }
 
         void Update()
@@ -27,6 +27,34 @@ namespace EnemyAI
             anim.SetFloat("X", dir.x);
             anim.SetFloat("Y", dir.y);
 
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("RunTree"))
+            {
+                
+                List<AnimatorClipInfo> clips = new List<AnimatorClipInfo>(anim.GetCurrentAnimatorClipInfo(0));
+                clips.Sort(SortByWeight);
+
+                if (clips[0].clip.name == "monster_walkdown")
+                {
+                    enemyFOV.transform.eulerAngles = new Vector3(0, 0, 270);
+                }
+                if (clips[0].clip.name == "monster_walkup")
+                {
+                    enemyFOV.transform.eulerAngles = new Vector3(0, 0, 90);
+                }
+                if (clips[0].clip.name == "monster_walkleft")
+                {
+                    enemyFOV.transform.eulerAngles = new Vector3(0, 0, 180);
+                }
+                if (clips[0].clip.name == "monster_walkright")
+                {
+                    enemyFOV.transform.eulerAngles = new Vector3(0, 0, 0);
+                }
+            }
+        }
+
+        private int SortByWeight(AnimatorClipInfo x, AnimatorClipInfo y)
+        {
+            return x.weight > y.weight ? -1 : 1;
         }
     }
 }
