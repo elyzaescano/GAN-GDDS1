0171df8add71368ca5f5a042837848c44331a5e6
MinXuan <liewminxuan@gmail.com>
2022-05-31 01:18:58 +0800

    Fixed PaperNote not changing text

--- GANgame-GDDS2/Assets/Script/PaperNotes.cs
+++ GANgame-GDDS2/Assets/Script/PaperNotes.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class PaperNotes : MonoBehaviour
 {
     public Text noteText;
-    public string note;
+    public string note; //ur input 
     public GameObject paperNote;
     bool viewNote = false;
     
@@ -19,7 +19,7 @@ public class PaperNotes : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        noteText.text = note;   
+          
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -30,11 +30,20 @@ public class PaperNotes : MonoBehaviour
         }
     }
 
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            viewNote = false;
+        }
+    }
+
     public void OpenNote()
     {
         if (viewNote)
         {
             paperNote.SetActive(true);
+            noteText.text = note;
             viewNote = false;
         }
     }
