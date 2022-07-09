d520858f07c014779720b07a7594bdfde3b77a3c
Greg <103052306+Thunderlord345@users.noreply.github.com>
2022-07-09 17:52:58 +0800

    first iteration of new menu
    stacia forgot a settings button

--- GANgame-GDDS2/Assets/UI Scripts/Menus/MenuSwitching.cs
+++ GANgame-GDDS2/Assets/UI Scripts/Menus/MenuSwitching.cs
@@ -10,13 +10,13 @@ public class MenuSwitching : MonoBehaviour
     public void Start()
     {
         audioManager = FindObjectOfType<AudioManager>();
-        
+
     }
     public void PlayGame()
     {
         SceneManager.LoadScene("House1");
-        
-        
+
+
     }
 
     public void LoadSaves()
@@ -27,21 +27,25 @@ public class MenuSwitching : MonoBehaviour
     public void Settings()
     {
         SceneManager.LoadScene("Settings");
-        
-       
-        
+
+
+
     }
 
     public void Credits()
     {
         SceneManager.LoadScene("Credits");
-        
+
     }
 
     public void ReturnToMainMenu()
     {
         SceneManager.LoadScene("Menu");
-        
+
     }
 
+    public void Quit()
+    {
+        Application.Quit();
+    }
 }
