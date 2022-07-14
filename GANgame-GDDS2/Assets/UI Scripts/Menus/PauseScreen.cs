18008247159913f503a483f73ffd2007e90c3916
Greg <103052306+Thunderlord345@users.noreply.github.com>
2022-07-15 00:30:13 +0800

    menubutton sound fixing

--- GANgame-GDDS2/Assets/UI Scripts/Menus/PauseScreen.cs
+++ GANgame-GDDS2/Assets/UI Scripts/Menus/PauseScreen.cs
@@ -8,7 +8,7 @@ public class PauseScreen : MonoBehaviour
 {
     public GameObject background;
     public bool canMove;
-    
+    public AudioSource menuButton;
 
     
 
@@ -16,6 +16,7 @@ public class PauseScreen : MonoBehaviour
     {
         Time.timeScale = 1f; //Resume game
         background.SetActive(false);
+        menuButton.Play();
         AudioManager.instance.UnPause("Level");
         canMove = true;
     }
@@ -24,18 +25,21 @@ public class PauseScreen : MonoBehaviour
     {
         Time.timeScale = 0f; //Freeze game
         background.SetActive(true);
+        menuButton.Play();
         AudioManager.instance.Pause("Level");
         canMove = false;
     }
 
    public void BacktoMenu()
     {
+        menuButton.Play();
         SceneManager.LoadScene("Menu");
         Resume();
     }
 
     public void Quit()
     {
+        menuButton.Play();
         Application.Quit();
     }
 
