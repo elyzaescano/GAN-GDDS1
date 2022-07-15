fa759d90aa1e5bf424ece3c8bc72cdfd14d14f7d
Greg <103052306+Thunderlord345@users.noreply.github.com>
2022-07-15 18:14:36 +0800

    volumesaving test

--- GANgame-GDDS2/Assets/UI Scripts/Menus/Audio/SetVolume.cs
+++ GANgame-GDDS2/Assets/UI Scripts/Menus/Audio/SetVolume.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Audio;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SetVolume : MonoBehaviour
 {
+    [SerializeField] private Slider volumeSlider = null;
+
     public AudioMixer mixer;
-   
+    float volume = 1.0f;
+
+    private void Start()
+    {
+        LoadValues();
+    }
     public void MenuBGM(float sliderValue)
     {
 
@@ -17,5 +25,20 @@ public class SetVolume : MonoBehaviour
     public void SoundEffects(float sliderValue)
     {
         mixer.SetFloat("SoundEffects", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SaveVolume()
+    {
+        float volumeValue = volumeSlider.value;
+        PlayerPrefs.SetFloat("VolumeLevel", volume);
+        LoadValues();
+
+    }
+
+    public void LoadValues()
+    {
+        float volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        volumeSlider.value = volumeValue;
+        AudioListener.volume = volumeValue;
     }
 }
