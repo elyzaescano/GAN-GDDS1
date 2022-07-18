ad7d1efbc5b9f3496464c40adb54777818be9cbf
srsbsns5 <elyzaescano@gmail.com>
2022-07-18 22:37:55 +0800

    part 1 of unlock message

--- GANgame-GDDS2/Assets/Character Scripts/UnlockBox.cs
+++ GANgame-GDDS2/Assets/Character Scripts/UnlockBox.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Dialogue;
 using UnityEngine;
 
 public class UnlockBox : MonoBehaviour
@@ -11,10 +12,13 @@ public class UnlockBox : MonoBehaviour
     public InventoryObject playerInventory;
     public ItemObject itemRequired;
     public EventManager em;
+
+    public GameObject dialoguebox;
+    public Conversation unlockMessage;
     // Start is called before the first frame update
     void Start()
     {
-        
+        dialoguebox = GameObject.FindGameObjectWithTag("Dialog").transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
@@ -40,6 +44,8 @@ public class UnlockBox : MonoBehaviour
     {
         if (canSpawn && this.enabled)
         {
+            if (unlockMessage != null) {dialoguebox.SetActive(true); 
+            dialoguebox.GetComponent<DialogDisplay>().conversation = unlockMessage;}
             box.itemNeeded = false;
             Destroy(GetComponent<UnlockBox>());
             Destroy(GetComponent<ItemSpawn>());
